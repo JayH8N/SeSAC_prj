535a3115c6f50501f5afd1c96849650216f2ffe1
JayH8N <dbwjdgns919@naver.com>
2023-08-16 21:09:59 +0900

    기존 구조체 삭제 및 기능 보완

--- SeSAC3Week4/VideoViewController.swift
+++ SeSAC3Week4/VideoViewController.swift
@@ -6,22 +6,20 @@
 //
 
 import UIKit
-import Alamofire
-import SwiftyJSON
 import Kingfisher
 
-struct Video {
-    let author: String
-    let date: String
-    let time: Int
-    let thumbnail: String
-    let title: String
-    let link: String
-
-    var contents: String {
-            return "\(author) | \(time)회\n\(date)"
-    }
-}
+//struct Video {
+//    let author: String
+//    let date: String
+//    let time: Int
+//    let thumbnail: String
+//    let title: String
+//    let link: String
+//
+//    var contents: String {
+//            return "\(author) | \(time)회\n\(date)"
+//    }
+//}
 
 
 //UITableViewDataSourcePrefetching : iOS10이상 사용 가능한 프로토콜, cellForRowAt메서드가 호출되기 전에 미리 호출됨
@@ -32,8 +30,12 @@ class VideoViewController: UIViewController {
     @IBOutlet var videoTableview: UITableView!
 
 
-    //lazy var documentList: [Document] = []
-    lazy var videoList: [Video] = []
+    lazy var documentList: [Document] = [] {
+        didSet {
+            videoTableview.reloadData()
+        }
+    }
+    //lazy var videoList: [Video] = []
     var page = 1 //스크롤 인식은 어떻게 하지? --> 담당 프로토콜 존재함
     var isEnd = false //현재 페이지가 마지막 페이지인지 점검하는 프로퍼티
 
@@ -103,17 +105,18 @@ extension VideoViewController: UITableViewDelegate, UITableViewDataSource, UITab
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: VideoTableViewCell.identifier) as? VideoTableViewCell else { return UITableViewCell()}
-        cell.titleLabel.text = videoList[indexPath.row].title
-        //cell.titleLabel.text = documentList[indexPath.row].title
-        cell.contentLabel.text = videoList[indexPath.row].contents
-        if let url = URL(string: videoList[indexPath.row].thumbnail) {
+        //cell.titleLabel.text = videoList[indexPath.row].title
+        cell.titleLabel.text = documentList[indexPath.row].title
+        //cell.contentLabel.text = videoList[indexPath.row].contents
+        cell.contentLabel.text = documentList[indexPath.row].contents
+        if let url = URL(string: documentList[indexPath.row].thumbnail) {
             cell.thumbnailImageView.kf.setImage(with: url)
         }
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return videoList.count
+        return documentList.count
     }
 
     //UITableViewDataSourcePrefetching
@@ -122,20 +125,28 @@ extension VideoViewController: UITableViewDelegate, UITableViewDataSource, UITab
     //카카오 제공 페이지 한도는 15이지만 컨텐츠마다 모두 15페이지는 넘지 않을 수도 있는 점 고려 -->
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         for indexPath in indexPaths {
-            if videoList.count - 1 == indexPath.row && page < 15 && !isEnd/*false라면*/ {
+            if documentList.count - 1 == indexPath.row && page < 15 && !isEnd/*false라면*/ {
                 page += 1
-                //callRequest(query: searchBar.text!, page: page)
+//                KakaoAPIManager.shared.callRequest(type: .video, query: searchBar.text!, page: page) { value in
+//
+//                    for i in value.documents {
+//                        let data = Video(author: i.author, date: i.datetime, time: i.playTime, thumbnail: i.thumbnail, title: i.title, link: i.url)
+//
+//
+//                        self.videoList.append(data)
+//                    }
+//
+//                    self.videoTableview.reloadData()
+//                }
                 KakaoAPIManager.shared.callRequest(type: .video, query: searchBar.text!, page: page) { value in
                     
                     for i in value.documents {
-                        let data = Video(author: i.author, date: i.datetime, time: i.playTime, thumbnail: i.thumbnail, title: i.title, link: i.url)
-                        
-                        
-                        self.videoList.append(data)
+                        let data = Document(author: i.author, datetime: i.datetime, playTime: i.playTime, thumbnail: i.thumbnail, title: i.title, url: i.url)
+                        self.documentList.append(data)
                     }
-                    
-                    self.videoTableview.reloadData()
                 }
+                
+                
             }
         }
     }
@@ -154,18 +165,25 @@ extension VideoViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
 
         page = 1 //검색시 마다 page를 1로 초기화 해야된다., 새로운 검색어이기 때문에 page를 1로 변경
-        videoList.removeAll()
+        documentList.removeAll()
 
         guard let query = searchBar.text else { return }
+//        KakaoAPIManager.shared.callRequest(type: .video, query: searchBar.text!, page: page) { value in
+//
+//            for i in value.documents {
+//                let data = Video(author: i.author, date: i.datetime, time: i.playTime, thumbnail: i.thumbnail, title: i.title, link: i.url)
+//
+//                self.videoList.append(data)
+//            }
+//
+//            self.videoTableview.reloadData()
+//        }
         KakaoAPIManager.shared.callRequest(type: .video, query: searchBar.text!, page: page) { value in
             
             for i in value.documents {
-                let data = Video(author: i.author, date: i.datetime, time: i.playTime, thumbnail: i.thumbnail, title: i.title, link: i.url)
-                
-                self.videoList.append(data)
+                let data = Document(author: i.author, datetime: i.datetime, playTime: i.playTime, thumbnail: i.thumbnail, title: i.title, url: i.url)
+                self.documentList.append(data)
             }
-            
-            self.videoTableview.reloadData()
         }
     }
 
@@ -188,6 +206,10 @@ struct Document: Codable {
     let thumbnail: String
     let title: String
     let url: String
+    
+    var contents: String {
+            return "\(author) | \(playTime)회\n\(datetime)"
+    }
 
     enum CodingKeys: String, CodingKey {
         case author, datetime
