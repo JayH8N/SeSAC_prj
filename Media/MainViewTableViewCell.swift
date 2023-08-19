9cbd5538f10978224ed2652a437113bb6e8660e0
JayH8N <dbwjdgns919@naver.com>
2023-08-19 17:48:42 +0900

    TableView Cell 레이아웃

---
+++ Media/MainViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewTableViewCell.swift
+//  Media
+//
+//  Created by hoon on 2023/08/19.
+//
+
+import UIKit
+
+class MainViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Sources/Extensions/Extension + MainView.swift
@@ -0,0 +1,13 @@
+//
+//  Extension + MainView.swift
+//  Media
+//
+//  Created by hoon on 2023/08/19.
+//
+
+import UIKit
+
+
+extension MainViewController: UITableView {
+    
+}
