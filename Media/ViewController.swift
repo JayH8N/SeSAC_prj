aeb44bdfdc729383996cb69a611c7f9ebcc0e25e
JayH8N <dbwjdgns919@naver.com>
2023-08-19 15:52:26 +0900

    Initial Commit

---
+++ Media/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Media
+//
+//  Created by hoon on 2023/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
