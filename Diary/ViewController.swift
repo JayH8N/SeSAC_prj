6a3504ed8420779d8c6f991d51a4dfd8884baf07
JayH8N <dbwjdgns919@naver.com>
2023-08-28 09:10:23 +0900

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by hoon on 2023/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
