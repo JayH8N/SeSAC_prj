acf97b791b6025c7de784e1a397fe2f9e0af57c4
JayH8N <dbwjdgns919@naver.com>
2023-11-06 19:48:54 +0900

    Base files Created

---
+++ App_Store/View/Base/BaseVC.swift
@@ -0,0 +1,26 @@
+//
+//  BaseVC.swift
+//  App_Store
+//
+//  Created by hoon on 11/6/23.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+        setConstraints()
+    }
+    
+    
+    func configureView() { }
+    
+    func setConstraints() { }
+    
+    
+}
